2d33416436dc7031ef7f1301e5daff2c0ca78b85
Jiahao Liu <jiahaoliuliu@gmail.com>
2013-12-30 15:12:37 +0100

    Created the first sample of the code.

---
+++ src/com/jiahaoliuliu/sherlockactionbarlab/HomeUpButton.java
@@ -0,0 +1,14 @@
+package com.jiahaoliuliu.sherlockactionbarlab;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeUpButton extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home_up_button);
+	}
+}
