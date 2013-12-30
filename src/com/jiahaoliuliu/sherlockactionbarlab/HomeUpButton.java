1ab1580c88013dc7f7ba68b5e6223b355f593182
Jiahao Liu <jiahaoliuliu@gmail.com>
2013-12-30 15:28:03 +0100

    Customized the actionBar home up button

--- src/com/jiahaoliuliu/sherlockactionbarlab/HomeUpButton.java
+++ src/com/jiahaoliuliu/sherlockactionbarlab/HomeUpButton.java
@@ -1,14 +1,18 @@
 package com.jiahaoliuliu.sherlockactionbarlab;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class HomeUpButton extends Activity {
+public class HomeUpButton extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_up_button);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 }
